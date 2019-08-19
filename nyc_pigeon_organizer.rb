ec36c26bb645e1a8331b87b00fc8dfe1ab963c16
Jonathan Koss <jonathan_koss@yahoo.com>
2019-08-19 23:13:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,9 @@
 def nyc_pigeon_organizer(pigeon_data)
  output_hash = {}
  pigeon_data.each do |key, value|
-   value.each 
+   value.each do |pushed_value, names_array|
+     
+   end 
  end 
  output_hash
 end 
